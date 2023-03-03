185c336cf2cf14053637849a3dea269682e8f53c
Joe Andrews <joeandrews92@gmail.com>
2023-03-03 14:53:50 -0800

    Create operate function to calculate equations and display the result

--- app.js
+++ app.js
@@ -3,22 +3,72 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const operate = function (operator, a, b){
-    if (operator == "+" ){
-        return add(a,b);
+let valueA = "";
+let valueB = "";
+let selectedOperator = "";
+
+const operate = function (){
+    store();
+    a = parseInt(valueA);
+    b = parseInt(valueB);
+    
+    if (selectedOperator === "+" ){
+        display.innerHTML = add(a,b);
+        valueA = display.innerHTML;
+    } else if (selectedOperator === "-") {
+        display.innerHTML = subtract(a,b);
+        valueA = display.innerHTML;
+    } else if (selectedOperator === "*") {
+        display.innerHTML = multiply(a,b);
+        valueA = display.innerHTML;
+    } else if (selectedOperator === "/") {
+        display.innerHTML = divide(a,b);
+        valueA = display.innerHTML;
     }
 }
 
 const display = document.getElementById("screen");
-const plus = document.getElementById("plus");
-plus.addEventListener("click", storeNum)
 
-function storeNum (numA) {
-    numA = display.innerHTML;
-;}
+// const onClick = (event) => {
+//     console.log(event.target.innerHTML);
+//     display.innerHTML += event.target.innerHTML;
+//   }
+//   window.addEventListener('click', onClick);
 
-const onClick = (event) => {
-    console.log(event.target.innerHTML);
+const nums = document.getElementsByClassName("nums");
+
+const numClick = (event) => {
+    if (display.innerHTML == selectedOperator) {
+        display.innerHTML = "";
+    }
     display.innerHTML += event.target.innerHTML;
   }
-  window.addEventListener('click', onClick);
+
+for (let num of nums) {
+    num.addEventListener("click", numClick);
+}
+
+const operators = document.getElementsByClassName("operator");
+
+const opsClick = (event) => {
+    store();
+    selectedOperator = event.target.innerHTML;
+    display.innerHTML = event.target.innerHTML;
+  }
+
+for (let operator of operators) {
+    operator.addEventListener("click", opsClick);
+}
+
+const store = function() { 
+    if (valueA == false){
+    valueA = display.innerHTML;
+    } else {
+    valueB = display.innerHTML;
+    }
+}
+
+const equals = document.getElementById("equals");
+equals.addEventListener("click", operate);
+
+
